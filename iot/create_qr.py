68f518144702a68ee7ab1ebe5e5ff4bc00d8dbc4
Ganesh <gane62672@gmail.com>
2020-10-30 17:54:32 +0530

    Add files via upload
    IOT files added

---
+++ iot/create_qr.py
@@ -0,0 +1,11 @@
+import qrcode
+from datetime import datetime
+input_data = str(datetime.now())
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode001.png')
